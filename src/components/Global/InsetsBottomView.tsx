7c9e92477abc808aca9f9da771f338979bd6eeed
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-26 02:30:35 +0200

    fix(types): insets bottom view

--- src/components/Global/InsetsBottomView.tsx
+++ src/components/Global/InsetsBottomView.tsx
@@ -1,11 +1,8 @@
-import React from "react";
 import { View } from "react-native";
-
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const InsetsBottomView = () => {
   const insets = useSafeAreaInsets();
-
   return <View style={{ height: insets.bottom + 16 }} />;
 };
 
