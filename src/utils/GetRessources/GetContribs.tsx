f18e9b35b724926f9b50b2219075ac99ac985f3f
Tryon <lucas.lavajo@gmail.com>
2024-08-28 12:32:26 +0200

    fix(types): :label: fix multiple types in GetContribs fonction & SettingsAbout view

--- src/utils/GetRessources/GetContribs.tsx
+++ src/utils/GetRessources/GetContribs.tsx
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import teams from '../../utils/data/teams.json';
+import axios from "axios";
+import teams from "../../utils/data/teams.json";
 
-interface Contributor {
+export interface Contributor {
   login: string;
   avatar_url: string;
   html_url: string;
 }
 
-export async function getContributors(): Promise<Contributor[]> {
+export async function getContributors (): Promise<Contributor[]> {
   try {
-    const { data: allContributors } = await axios.get<Contributor[]>('https://api.github.com/repos/PapillonApp/Papillon/contributors');
-    
+    const { data: allContributors } = await axios.get<Contributor[]>("https://api.github.com/repos/PapillonApp/Papillon/contributors");
+
     const teamGithubUrls = new Set(
       teams
-        .filter(({ github }) => github)
-        .map(({ github }) => github.toLowerCase())
+        .filter((team): team is { name: string; description: string; link: string; ppimage: string; github: string } => Boolean(team.github))
+        .map(({ github }) => github?.toLowerCase() ?? "")
     );
 
     return allContributors.filter(({ html_url }) =>
-      !teamGithubUrls.has(new URL(html_url).hostname.replace('www.', ''))
+      !teamGithubUrls.has(new URL(html_url).hostname.replace("www.", ""))
     );
   } catch (error) {
-    console.error('Erreur lors de la récupération des contributeurs:', error);
+    console.error("Erreur lors de la récupération des contributeurs:", error);
     return [];
   }
 }
