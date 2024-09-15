59feec2ea72368ebfb47c51164385d7c7cfa3a70
camarm-dev <contact@camarm.dev>
2024-09-15 16:16:14 +0200

    feat(account): POC implementation of ED account reloading

---
+++ src/services/ecoledirecte/reload.ts
@@ -0,0 +1,18 @@
+import type { EcoleDirecteAccount } from "@/stores/account/types";
+import ecoledirecte from "pawdirecte";
+import { Reconnected } from "../reload-account";
+
+export const reload = async (account: EcoleDirecteAccount): Promise<Reconnected<EcoleDirecteAccount>> => {
+  const authentication = account.authentication;
+
+  // TODO: refresh returns a list of accounts. Support multiple ED accounts
+  const refresh = await ecoledirecte.refresh(authentication.session, authentication.session.token as string, authentication.account.kind);
+  const refreshedAccount = refresh[0];
+  return {
+    instance: account.instance,
+    authentication: {
+      ...authentication,
+      ...refreshedAccount
+    }
+  };
+};
