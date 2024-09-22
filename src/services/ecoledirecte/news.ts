dce9a6064c890e6986675194c3bed9c67e811715
camarm-dev <contact@camarm.dev>
2024-09-22 15:05:25 +0200

    fix(news): poc of news using pawdirecte

--- src/services/ecoledirecte/news.ts
+++ src/services/ecoledirecte/news.ts
@@ -3,25 +3,22 @@ import { Information } from "../shared/Information";
 import { ErrorServiceUnauthenticated } from "../shared/errors";
 import ecoledirecte from "pawdirecte";
 
-// WAITING FOR PAWDIRECTE UPDATE. THIS WOULD BE UNCOMMENTED AFTER THE LATEST PAWDIRECTE RELEASE
-
-// const parseInformation = (i: ecoledirecte.HomepageTimelineItem): Information => ({
-//   id: i.id,
-//   title: i.title,
-//   date: i.startDate,
-//   acknowledged: i.acknowledged,
-//   attachments: [],
-//   content: i.content,
-//   author: i.author,
-//   category: i.category.name,
-//   read: i.read,
-// });
+const parseInformation = (i: ecoledirecte.HomepageTimelineItem): Information => ({
+  id: i.id,
+  title: "",
+  date: i.creationDate,
+  acknowledged: false,
+  attachments: [],
+  content: i.content,
+  author: i.authorName,
+  category: "",
+  read: false,
+});
 
 export const getNews = async (account: EcoleDirecteAccount): Promise<Information[]> => {
   if (!account.instance)
     throw new ErrorServiceUnauthenticated("ecoledirecte");
 
-  // const news = await ecoledirecte.studentHomepageTimeline(account.instance);
-  // return news.map(parseInformation);
-  return [];
+  const news = await ecoledirecte.studentHomepageTimeline(account.authentication.session, account.authentication.account);
+  return news.map(parseInformation);
 };
