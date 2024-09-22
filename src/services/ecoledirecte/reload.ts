7902f726c85a41c0f2ae6d37fd54a49a88e18633
camarm-dev <contact@camarm.dev>
2024-09-22 15:46:13 +0200

    fix(reload): implemented reloading for ecoledirecte (camarm-dev/papillon-v7#3)

--- src/services/ecoledirecte/reload.ts
+++ src/services/ecoledirecte/reload.ts
@@ -6,7 +6,7 @@ export const reload = async (account: EcoleDirecteAccount): Promise<Reconnected<
   const authentication = account.authentication;
 
   // TODO: refresh returns a list of accounts. Support multiple ED accounts
-  const refresh = await ecoledirecte.refresh(authentication.session, authentication.session.token as string, authentication.account.kind);
+  const refresh = await ecoledirecte.refresh(authentication.session, authentication.session.accessToken as string, authentication.account.kind);
   const refreshedAccount = refresh[0];
 
   return {
