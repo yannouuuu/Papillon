ae1a91dfe2897bcf0a7bcf773fdd9a03c695f7e4
Yanice <git@yanice.me>
2024-09-29 09:33:06 +0200

    feat(homeworks): implemented homeworks

--- src/services/ecoledirecte/homework.ts
+++ src/services/ecoledirecte/homework.ts
@@ -3,21 +3,23 @@ import { type Homework } from "@/services/shared/Homework";
 import { EcoleDirecteAccount } from "@/stores/account/types";
 import { ErrorServiceUnauthenticated } from "../shared/errors";
 import { weekNumberToDaysList } from "@/utils/epochWeekNumber";
+import { log } from "@/utils/logger/logger";
 
 export const getHomeworkForWeek = async (account: EcoleDirecteAccount, weekNumber: number): Promise<Homework[]> => {
   if (!account.authentication.session)
     throw new ErrorServiceUnauthenticated("ecoledirecte");
 
-  const weekdays  = weekNumberToDaysList(weekNumber);
+  const weekdays = weekNumberToDaysList(weekNumber);
   const response: Homework[] = [];
 
   for (const date of weekdays) {
-    const formattedDate = date.getFullYear + "-" + date.getMonth + "-" + date.getDay;
+    const formattedDate = formatDate(date);
+
     const homeworks = await ecoledirecte.studentHomeworks(account.authentication.session, account.authentication.account, formattedDate);
     for (const homework of homeworks) {
       response.push({
         attachments: [],
-        color: "", // TODO
+        color: "#000000", // TODO
         content: homework.content,
         done: homework.done,
         due: date.getTime(),
@@ -26,11 +28,20 @@ export const getHomeworkForWeek = async (account: EcoleDirecteAccount, weekNumbe
       });
     }
   }
-  return response;
+  return response.filter(hw => hw.content !== "");
 };
 
 export const toggleHomeworkState = async (account: EcoleDirecteAccount, homework: Homework): Promise<void> => {
   if (!account.authentication.session)
     throw new ErrorServiceUnauthenticated("ecoledirecte");
-  await ecoledirecte.setHomeworkState(account.authentication.session, account.authentication.account, homework.id, !homework.done);
+  await ecoledirecte.setHomeworkState(account.authentication.session, account.authentication.account, Number(homework.id), !homework.done);
+  log(`Homework ${homework.id} marked as ${homework.done ? "not done" : "done"}.`, "ED->toggleHomeworkState");
+};
+
+const formatDate = (date: Date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // months are 0-indexed, so +1
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 };
--- src/services/homework.ts
+++ src/services/homework.ts
@@ -13,42 +13,43 @@ import { checkIfSkoSupported } from "./skolengo/default-personalization";
 export async function updateHomeworkForWeekInCache <T extends Account> (account: T, date: Date): Promise<void> {
   let homeworks: Homework[] = [];
 
-  try {
-    switch (account.service) {
-      case AccountService.Pronote: {
-        const { getHomeworkForWeek } = await import("./pronote/homework");
-        const weekNumber = translateToWeekNumber(date, account.instance?.instance.firstDate || pronoteFirstDate);
-        homeworks = await getHomeworkForWeek(account, weekNumber);
-        break;
-      }
-      case AccountService.Skolengo: {
-        if(!checkIfSkoSupported(account, "Homeworks")) {
-          error("[updateHomeworkForWeekInCache]: This Skolengo instance doesn't support Homeworks.", "skolengo");
-          break;
-        }
-        const { getHomeworkForWeek } = await import("./skolengo/data/homework");
-        const weekNumber = dateToEpochWeekNumber(date);
-        homeworks = await getHomeworkForWeek(account, weekNumber);
-        break;
-      }
-      case AccountService.EcoleDirecte: {
-        const { getHomeworkForWeek } = await import("./ecoledirecte/homework");
-        const weekNumber = dateToEpochWeekNumber(date);
-        homeworks = await getHomeworkForWeek(account, weekNumber);
-      }
-      case AccountService.Local: {
-        homeworks = [];
-        break;
-      }
-      default:
-        console.info(`[updateHomeworkForWeekInCache]: updating to empty since ${account.service} not implemented.`);
+  //try {
+  switch (account.service) {
+    case AccountService.Pronote: {
+      const { getHomeworkForWeek } = await import("./pronote/homework");
+      const weekNumber = translateToWeekNumber(date, account.instance?.instance.firstDate || pronoteFirstDate);
+      homeworks = await getHomeworkForWeek(account, weekNumber);
+      break;
     }
+    case AccountService.Skolengo: {
+      if(!checkIfSkoSupported(account, "Homeworks")) {
+        error("[updateHomeworkForWeekInCache]: This Skolengo instance doesn't support Homeworks.", "skolengo");
+        break;
+      }
+      const { getHomeworkForWeek } = await import("./skolengo/data/homework");
+      const weekNumber = dateToEpochWeekNumber(date);
+      homeworks = await getHomeworkForWeek(account, weekNumber);
+      break;
+    }
+    case AccountService.EcoleDirecte: {
+      const { getHomeworkForWeek } = await import("./ecoledirecte/homework");
+      const weekNumber = dateToEpochWeekNumber(date);
+      homeworks = await getHomeworkForWeek(account, weekNumber);
+      break;
+    }
+    case AccountService.Local: {
+      homeworks = [];
+      break;
+    }
+    default:
+      console.info(`[updateHomeworkForWeekInCache]: updating to empty since ${account.service} not implemented.`);
+  }
 
-    useHomeworkStore.getState().updateHomeworks(dateToEpochWeekNumber(date), homeworks);
-  }
-  catch (err) {
-    error("not updated, see:" + err, "updateHomeworkForWeekInCache");
-  }
+  useHomeworkStore.getState().updateHomeworks(dateToEpochWeekNumber(date), homeworks);
+  //}
+  //catch (err) {
+  // error("not updated, see:" + err, "updateHomeworkForWeekInCache");
+  // }
 }
 
 export async function toggleHomeworkState <T extends Account> (account: T, homework: Homework): Promise<void> {
