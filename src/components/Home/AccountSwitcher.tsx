9b560b043da227f1e13521d862dd01d85fd6a535
Rémy Godet <godetremy@free.fr>
2024-10-01 10:57:07 +0200

    feat(AccountSwitcher): AccountSwitcher is redisigned to keep constant design with the homework and timetable page

--- src/components/Home/AccountSwitcher.tsx
+++ src/components/Home/AccountSwitcher.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import { ChevronDown } from "lucide-react-native";
 
 import Reanimated, {
-  FadeIn,
-  FadeOut,
-  LinearTransition,
+  interpolateColor,
+  LinearTransition, useAnimatedStyle,
   ZoomIn,
   ZoomOut,
 } from "react-native-reanimated";
@@ -15,13 +14,14 @@ import { useCurrentAccount } from "@/stores/account";
 import { defaultProfilePicture } from "@/utils/ui/default-profile-picture";
 import PapillonSpinner from "../Global/PapillonSpinner";
 import { animPapillon } from "@/utils/ui/animations";
+import Animated from "react-native-reanimated";
 
 const AccountSwitcher: React.FC<{
   small?: boolean,
   scrolled?: boolean,
   translationY?: Reanimated.SharedValue<number>,
   loading?: boolean,
-}> = ({ small, scrolled, translationY, loading }) => {
+}> = ({ small, translationY, loading }) => {
   const theme = useTheme();
   const { colors } = theme;
 
@@ -30,125 +30,141 @@ const AccountSwitcher: React.FC<{
   const shouldHideName = account.personalization.hideNameOnHomeScreen || false;
   const shouldHidePicture = account.personalization.hideProfilePicOnHomeScreen || false;
 
+  const borderAnimatedStyle = useAnimatedStyle(() => ({
+    borderWidth: 1,
+    borderRadius: 80,
+    borderColor: interpolateColor(
+      translationY.value,
+      [200, 251],
+      ["#ffffff50", colors.border],
+    ),
+    backgroundColor: interpolateColor(
+      translationY.value,
+      [200, 251],
+      ["#ffffff30", "transparent"],
+    ),
+  }));
+
+  const textAnimatedStyle = useAnimatedStyle(() => ({
+    color: interpolateColor(
+      translationY.value,
+      [200, 251],
+      ["#FFF", colors.text],
+    ),
+    fontSize: 16,
+    fontFamily: "semibold",
+    maxWidth: 140,
+  }));
+
+
+  const AnimatedChevronDown = Animated.createAnimatedComponent(ChevronDown);
+  const iconAnimatedStyle = useAnimatedStyle(() => ({
+    color: interpolateColor(
+      translationY.value,
+      [200, 251],
+      ["#FFF", colors.text],
+    ),
+    marginLeft: -6,
+  }));
+
   return (
-    <Reanimated.View
-      layout={animPapillon(LinearTransition)}
-      style={[
-        styles.accountSwitcher,
-        {
-          backgroundColor: colors.background,
-        },
-        loading && {
-          shadowOpacity: 0,
-        },
-        small && {
-          paddingHorizontal: 0,
-          shadowOpacity: 0,
-          elevation: 0,
-          borderRadius: 0,
-          paddingVertical: 0,
-          backgroundColor: "transparent",
-        }
-      ]}
-    >
-
-      {!small && (
-        <Reanimated.View
-          style={[{
-            position: "absolute",
-            backgroundColor: theme.dark ? theme.colors.primary + "09" : theme.colors.primary + "11",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            borderRadius: 10,
-          }]}
-          entering={FadeIn.duration(150)}
-          exiting={FadeOut.duration(150)}
-        />
-      )}
-
+    <Reanimated.View style={borderAnimatedStyle}>
       <Reanimated.View
-        style={[
-          {
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 12,
-          },
-        ]}
         layout={animPapillon(LinearTransition)}
+        style={[
+          styles.accountSwitcher,
+          loading && {
+            shadowOpacity: 0,
+          },
+          small && {
+            paddingHorizontal: 0,
+            shadowOpacity: 0,
+            elevation: 0,
+            borderRadius: 0,
+            paddingVertical: 0,
+            backgroundColor: "transparent",
+          }
+        ]}
       >
-        {!shouldHidePicture ? (
-          <Image
-            source={(account.personalization.profilePictureB64 && account.personalization.profilePictureB64.trim() !== "") ? { uri: account.personalization.profilePictureB64 } : defaultProfilePicture(account.service)}
-            style={[
-              styles.avatar,
-              {
-                backgroundColor: colors.text + "22",
-                height: small ? 30 : 28,
-                width: small ? 30 : 28,
-              }
-            ]}
-          />
-        ) : (
-          <View style={[
-            {
-              marginLeft: -8,
-              height: small ? 30 : 28,
-            }
-          ]} />
-        )}
-
-        <Text
-          style={[styles.accountSwitcherText, {
-            color: colors.text,
-          }]}
-
-          numberOfLines={1}
-          ellipsizeMode="tail"
-        >
-          {account.studentName ? (
-            account.studentName?.first + (shouldHideName ? "" : " " + account.studentName.last)
-          ) : "Mon compte"}
-        </Text>
-
-        {loading && (
-          <PapillonSpinner
-            size={20}
-            strokeWidth={3}
-            color={colors.text}
-            animated
-            entering={animPapillon(ZoomIn)}
-            exiting={animPapillon(ZoomOut)}
-          />
-        )}
-
         <Reanimated.View
+          style={[
+            {
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 12,
+            },
+          ]}
           layout={animPapillon(LinearTransition)}
         >
-          <ChevronDown
-            size={24}
-            strokeWidth={2.3}
-            color={colors.text + "55"}
-            style={{ marginLeft: -6 }}
-          />
+          {!shouldHidePicture ? (
+            <Image
+              source={(account.personalization.profilePictureB64 && account.personalization.profilePictureB64.trim() !== "") ? { uri: account.personalization.profilePictureB64 } : defaultProfilePicture(account.service)}
+              style={[
+                styles.avatar,
+                {
+                  backgroundColor: colors.text + "22",
+                  height: small ? 30 : 28,
+                  width: small ? 30 : 28,
+                }
+              ]}
+            />
+          ) : (
+            <View style={[
+              {
+                marginLeft: -8,
+                height: small ? 30 : 28,
+              }
+            ]} />
+          )}
+
+          <Reanimated.Text
+            style={textAnimatedStyle}
+            numberOfLines={1}
+            ellipsizeMode="tail"
+          >
+            {account.studentName ? (
+              account.studentName?.first + (shouldHideName ? "" : " " + account.studentName.last)
+            ) : "Mon compte"}
+          </Reanimated.Text>
+
+          {loading && (
+            <PapillonSpinner
+              size={20}
+              strokeWidth={3}
+              color={colors.text}
+              animated
+              entering={animPapillon(ZoomIn)}
+              exiting={animPapillon(ZoomOut)}
+            />
+          )}
+
+          <Reanimated.View
+            layout={animPapillon(LinearTransition)}
+          >
+            <AnimatedChevronDown
+              size={24}
+              strokeWidth={2.3}
+              style={iconAnimatedStyle}
+            />
+          </Reanimated.View>
         </Reanimated.View>
       </Reanimated.View>
-
     </Reanimated.View>
   );
 };
 
 const styles = StyleSheet.create({
   accountSwitcher: {
-    alignSelf: "flex-start",
     flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 8,
+    paddingHorizontal: 6,
+    height: 40,
+    borderRadius: 800,
+    alignSelf: "flex-start",
+    overflow: "hidden",
     paddingVertical: 6,
-    gap: 12,
-    borderRadius: 10,
-    borderCurve: "continuous",
+    gap: 6,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -167,9 +183,7 @@ const styles = StyleSheet.create({
   },
 
   accountSwitcherText: {
-    fontSize: 16,
-    fontFamily: "semibold",
-    maxWidth: 140,
+
   },
 });
 
