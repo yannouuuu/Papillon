7248cb2056d53bcb53f256ea6f6260d3c5894529
camarm-dev <contact@camarm.dev>
2024-10-04 21:10:51 +0200

    fix: function simplification for `download-as-base64.ts`

--- src/utils/external/download-as-base64.ts
+++ src/utils/external/download-as-base64.ts
@@ -1,12 +1,9 @@
 export default async function downloadAsBase64 (url: string, headers?: Record<string, string>, method = "GET", body: Record<string, any> = {}): Promise<string> {
-  let response: Response;
-  if (method === "GET")  {
-    response = await fetch(url, { headers });
-
-  } else {
-    response = await fetch(url, { headers, method, body });
-
-  }
+  const response = await fetch(url, {
+    headers,
+    method,
+    body: body as BodyInit
+  });
   const blob = await response.blob();
 
   return new Promise<string>(resolve => {
