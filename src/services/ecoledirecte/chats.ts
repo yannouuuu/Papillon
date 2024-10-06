3d052be0781f5e8389c3a373ba23c3c9ba5f8467
camarm-dev <contact@camarm.dev>
2024-10-06 11:43:45 +0200

    fix: moved format date to file and deleted unused function

--- src/services/ecoledirecte/chats.ts
+++ src/services/ecoledirecte/chats.ts
@@ -37,11 +37,3 @@ export const getChatMessages = async (account: EcoleDirecteAccount, chat: Chat):
     }))
   };
 };
-
-const formatDate = (date: Date) => {
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // months are 0-indexed, so +1
-  const day = date.getDate().toString().padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-};
