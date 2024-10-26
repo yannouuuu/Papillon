dd4c63f3eb9a28a9f82ac9a454f1a9795b448eb0
camarm-dev <contact@camarm.dev>
2024-10-26 17:07:32 +0200

    fix: import was causing module typecheck error

--- src/background/Notifications.ts
+++ src/background/Notifications.ts
@@ -1,5 +1,5 @@
 import { expoGoWrapper } from "@/utils/native/expoGoAlert";
-import {Notification} from "@notifee/react-native/src/types/Notification";
+import {Notification} from "@notifee/react-native";
 
 const requestNotificationPermission = async () => {
   return expoGoWrapper(async () => {
